1cee02690b64b0c5fd05e8fd34516e072582b8d7
UsrRmt <UsrRmt@DESKTOP-2M43GN8>
2019-12-06 02:55:59 -0200

    ajustes no projeto e .gitignore

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,25 +1,33 @@
-import cucumber.api.PendingException;
+import java.util.List;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import gherkin.formatter.model.Comment;
 
 public class AprenderCucumber {
 	@Given("^que criei o arquivo corretamente$")
 	public void que_criei_o_arquivo_corretamente() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new RuntimeException();
+	    System.out.println("Executou passo criei arquivo");
 	}
 
 	@When("^executá-lo$")
 	public void execut_lo() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+//	    throw new PendingException();
 	}
 
 	@Then("^a especificação deve finalizar com sucesso$")
 	public void a_Especifica_o_deve_finalizar_com_sucesso() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+//	    throw new PendingException();
+	}
+	@Before(order=1)
+	public void pegaInformacoesScenario(Scenario cenario) {
+		System.out.println( getClass().getName() +": "+ cenario.getId());
+		
 	}
 
 
---
+++ src/test/java/TesteInfoCenario.java
@@ -0,0 +1,11 @@
+import cucumber.api.Scenario;
+import cucumber.api.java.Before;
+
+public class TesteInfoCenario {
+	@Before(order=2)
+	public void recebeInformacoes(Scenario cenario) {
+		String id = cenario.getId();
+		System.out.println(id);
+	}
+	
+}
