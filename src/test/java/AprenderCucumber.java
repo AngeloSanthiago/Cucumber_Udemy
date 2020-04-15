3fe0939c9a1a628e2f37e0b5b2ce10866cbc018f
angel <angel@10.161.214.208>
2020-04-15 16:12:10 -0300

    Implementação completa video 08

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,30 +1,28 @@
 import cucumber.api.PendingException;
-import cucumber.api.Scenario;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
-	@Given("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() throws Throwable {
-//	    System.out.println("Executou passo criei arquivo");
-//	    throw new Exception();
-//		throw new RuntimeException();
-	    throw new PendingException();
-	}
-
-	@When("^executá-lo$")
-	public void execut_lo() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+//	@Dado("^que criei o arquivo corretamente$")
+//	public void que_criei_o_arquivo_corretamente() throws Throwable {
+////	    System.out.println("Executou passo criei arquivo");
+////	    throw new Exception();
+////		throw new RuntimeException();
 //	    throw new PendingException();
-	}
-
-	@Then("^a especificação deve finalizar com sucesso$")
-	public void a_Especifica_o_deve_finalizar_com_sucesso() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
+//	}
+//
+//	@Quando("^executá-lo$")
+//	public void execut_lo() throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+////	    throw new PendingException();
+//	}
+//
+//	@Então("^a especificação deve finalizar com sucesso$")
+//	public void a_Especifica_o_deve_finalizar_com_sucesso() throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+//	}
 	    
 //	@Before(order=1)
 //	public void pegaInformacoesScenario(Scenario cenario) {
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -4,7 +4,7 @@ Funcionalidade: Aprender Cucumber
   Eu quero aprender a utilizar Cucumber
   Para que eu possa automatizar critérios de aceitação
   
-Scenario: Deve executar especificação
+Cenário: Deve executar especificação
   Dado que criei o arquivo corretamente
   Quando executá-lo
   Entao a especificação deve finalizar com sucesso
@@ -12,6 +12,6 @@ Scenario: Deve executar especificação
 # Os cinco Estatos de execução do Cucumber: 
 # Passou(Passed) - Executou com sucesso;
 # Falhou(Failed) - Lançou uma excessão Java;
-# Ecapou(Skipped) - ;
+# Ecapou(Skipped) - O passo está implementado mas, não foi executado;
 # Pending - quando a excessão de pendencia de implementação foi lançada;
 # Undefined - quando o passo não foi mapeado através do Gerkin para código java. Sempre é lançado independentemento dos outros status
